3918c265d7b3819a640a6dcd20cb16a26a54a51c
shreeya-sengupta <shreeya-sengupta@users.noreply.github.com>
2025-05-05 08:17:46 +0530

    update to tokenise sentences

--- app/bpe.py
+++ app/bpe.py
@@ -7,15 +7,20 @@ from collections import defaultdict
 final_vocab = ['Walker </w>', 'w alke d </w>', 'a </w>', 'l o n g </w>', 'w alk </w>', 'a t </w>', 's o m e u n k n o w n b e a c h </w>']
 learned_merges = [('a', 'l'), ('al', 'k'), ('alk', 'e'), ('W', 'alke'), ('Walke', 'r')]
 
+
 def apply_bpe(test_word, merges):
     test_tokens = list(test_word) + ['</w>']
     i = 0
     while i < len(test_tokens) - 1:
         pair = (test_tokens[i], test_tokens[i+1])
-        if pair in reversed(merges):
+        if pair in merges:
             test_tokens[i] = ''.join(pair)
             del test_tokens[i+1]
             i = max(i-1, 0)  # re-check merged token with previous one
         else:
             i += 1
-    return test_tokens
+    
+    tokenised_string = ' '.join(test_tokens)
+
+    return tokenised_string
+
